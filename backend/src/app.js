f7aab64e6b2448d99a37bc1488477cb87580c517
parag chouhan <paragchouhan4@gmail.com>
2026-06-10 21:57:28 +0530

    again testing

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,10 @@ const newsRoutes = require('./routes/news.routes');
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    "https://vyapaar-saarthi.vercel.app"
+  ],
   credentials: true,
 }));
 app.use(express.json());

b9c4e9290cfed61e0418b6040e944b09f94481f0
parag chouhan <paragchouhan4@gmail.com>
2026-07-17 07:32:46 +0530

    news feature updated

--- backend/src/cron/news.cron.js
+++ backend/src/cron/news.cron.js
@@ -1,34 +1,37 @@
-const cron = require("node-cron");
+    const cron = require("node-cron");
 
-const News = require("../models/news");
-const { fetchBusinessNews } = require("../services/news.services");
+    const News = require("../models/news");
+    const { fetchBusinessNews } = require("../services/news.services");
 
-cron.schedule("*/30 * * * *", async () => {
-    try {
-        console.log("Starting news fetch...");
-        
-        const articles = await fetchBusinessNews();
-        console.log("Articles fetched:", articles.length);
+    cron.schedule("*/30 * * * *", async () => {
+        try {
+            console.log("Starting news fetch...");
+            
+            const articles = await fetchBusinessNews();
 
-        /* await News.deleteMany({});
-        console.log("Old news deleted"); */
+            if(!articles || articles.length === 0) {
+                console.log("No articles fetched, skipping update.");
+                return;
+            }
+            await News.deleteMany({});
+            console.log("Old news deleted");
 
-        await News.insertMany(
-            articles.map(article => ({
-                title: article.title,
-                description: article.description,
-                imageUrl: article.urlToImage,
-                source: article.source.name,
-                publishedAt: article.publishedAt,
-                url: article.url,
-                category: "business"
-            }))
-        );
+            await News.insertMany(
+                articles.map(article => ({
+                    title: article.title,
+                    description: article.description,
+                    imageUrl: article.urlToImage,
+                    source: article.source.name,
+                    publishedAt: article.publishedAt,
+                    url: article.url,
+                    category: "business"
+                }))
+            );
 
-        console.log("Business News Updated Successfully");
-    } catch (error) {
-        console.error("Error updating news:", error.message);
-    }
-});
+            console.log("Business News Updated Successfully");
+        } catch (error) {
+            console.error("Error updating news:", error.message);
+        }
+    });
 
-console.log("News cron job initialized");
+    console.log("News cron job initialized");
